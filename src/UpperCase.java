6da031a1a2228bfaf5ae60b8dadc0842e0707f41
Kieran Cornish <kcornish@spartaglobal.com>
2019-10-14 12:00:03 +0100

    commiting upperCase java

---
+++ src/UpperCase.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class UpperCase {
+    public static void main(String[] args) {
+
+            String changeCase = "text to change";
+            System.out.println(changeCase);
+
+            String result;
+            result = changeCase.toUpperCase();
+
+            System.out.println(result);
+    }
+
+}
